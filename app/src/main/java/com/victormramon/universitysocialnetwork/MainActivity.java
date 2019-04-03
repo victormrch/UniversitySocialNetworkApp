894788cb05ca7ee06c5cfcf844031124935a9d62
victormrch <victormanuelchica@gmail.com>
2019-04-03 19:01:40 +0200

    Diseño Principal University Social Network App

--- app/src/main/java/com/victormramon/universitysocialnetwork/MainActivity.java
+++ app/src/main/java/com/victormramon/universitysocialnetwork/MainActivity.java
@@ -73,17 +73,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_profile) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_friends) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_groups) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_post) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_new_friend) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_new_group) {
 
         }
 
