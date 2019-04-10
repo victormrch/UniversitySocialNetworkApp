375960dc216d27804ab57a2ccec5c2f22f550a62
Jesus <jesustrujillo5023@gmail.com>
2019-04-10 12:34:23 +0200

    boton añadir amigos en Friends funcionando

--- app/src/main/java/com/victormramon/universitysocialnetwork/Friends.java
+++ app/src/main/java/com/victormramon/universitysocialnetwork/Friends.java
@@ -4,12 +4,15 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.gson.Gson;
 import com.victormramon.universitysocialnetwork.callback.Callback;
@@ -36,9 +39,9 @@ public class Friends extends AppCompatActivity implements Callback {
     public void start() {
         this.user = (Usuario) getIntent().getExtras()
                 .getSerializable(getString(R.string.key_userLogged));
-        //gson = new GsonBuilder().create();
-        //getUser();
+
         chargeRV(this.user, R.layout.friends_item, R.id.rvFriends);
+        preparedAddFriendBtn();
     }
 
     public void getUser() {
@@ -86,6 +89,20 @@ public class Friends extends AppCompatActivity implements Callback {
         Intent intent = new Intent();
         setResult(Activity.RESULT_OK);
         finish();
+    }
+
+    private void preparedAddFriendBtn() {
+        FloatingActionButton btnAddFriend = findViewById(R.id.floatingFollowFriends);
+        btnAddFriend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle args = new Bundle();
+                args.putSerializable(getString(R.string.key_userLogged), user);
+                Intent intent = new Intent(Friends.this, AddRelationships.class);
+                intent.putExtras(args);
+                startActivityForResult(intent, 1);
+            }
+        });
     }
 
 }
